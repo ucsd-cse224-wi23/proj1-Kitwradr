3f25bc444119268a3eaf3357f8205c9d9aaa94a2
Suhas <suhas.2ab@gmail.com>
2023-01-22 18:41:03 -0800

    changes

--- src/netsort.go
+++ src/netsort.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"gopkg.in/yaml.v2"
@@ -19,6 +21,12 @@ type ServerConfigs struct {
 		Host     string `yaml:"host"`
 		Port     string `yaml:"port"`
 	} `yaml:"servers"`
+}
+
+type Record struct {
+	Complete byte
+	Key      [10]byte
+	Value    [90]byte
 }
 
 func readServerConfigs(configPath string) ServerConfigs {
@@ -38,13 +46,12 @@ const (
 	Proto          = "tcp"
 	Host           = "localhost"
 	Port           = "8080"
-	WorldSize      = 7
 	MaxMessageSize = 101
 	MaxRetries     = 3
 )
 
-func listenForClientConnections(write_only_ch chan<- string, server_id int, address string) {
-	//server_address := Host + ":" + Port
+func listenForClientConnections(write_only_ch chan<- Record, server_id int, address string) {
+
 	fmt.Println("Starting " + Proto + " server on " + address)
 	listener, err := net.Listen(Proto, address)
 	if err != nil {
@@ -64,18 +71,33 @@ func listenForClientConnections(write_only_ch chan<- string, server_id int, addr
 	}
 }
 
-func handleClientConnection(conn net.Conn, write_only_ch chan<- string, server_id int) {
+func handleClientConnection(conn net.Conn, write_only_ch chan<- Record, server_id int) {
 
-	client_msg_buf := make([]byte, MaxMessageSize)
-	bytes_read, err := conn.Read(client_msg_buf)
+	//client_msg_buf := make([]byte, MaxMessageSize)
+	// bytes_read, err := conn.Read(client_msg_buf)
+
+	var record Record
+	err := binary.Read(conn, binary.BigEndian, &record)
 	if err != nil {
 		fmt.Println("read error")
 		os.Exit(1)
 	}
-	message := string(client_msg_buf[0:bytes_read])
-	fmt.Println("My server ID", server_id, "message received", message)
-	write_only_ch <- message
+	fmt.Println("Record received on server ", server_id)
+	write_only_ch <- record
 	conn.Close()
+}
+
+func readFullyFromClient(ch chan Record, records []Record, wg *sync.WaitGroup) {
+	for {
+		record := <-ch
+		fmt.Println("Receiving on")
+		if record.Complete == 1 {
+			wg.Done()
+			break
+		} else {
+			records = append(records, record)
+		}
+	}
 }
 
 func main() {
@@ -108,10 +130,11 @@ func main() {
 
 	fmt.Println("Number of bits for mask", number_of_bits)
 
-	ch := make(chan string)
+	ch := make(chan Record)
 	defer close(ch)
 
 	var self_address string
+	// Listening on this server
 	for _, server := range scs.Servers {
 		if serverId == server.ServerId {
 			self_address = server.Host + ":" + server.Port
@@ -122,35 +145,63 @@ func main() {
 
 	go listenForClientConnections(ch, serverId, self_address)
 
+	// Getting file and segregating
+	file, err := os.Open(os.Args[2])
+
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Connecting to rest of the servers
 	for _, server := range scs.Servers {
 		//fmt.Println("Server configs", index, server)
+		//fmt.Println(serverId, server.ServerId, serverId != server.ServerId)
 		if server.ServerId != serverId {
-			go ConnectToServer(server.Host, server.Port, server.ServerId)
+			go ConnectToServer(server.Host, server.Port, serverId, file)
 		}
 	}
+	messages_received := 0
+
+	var my_records []Record
+	var wg sync.WaitGroup
+	wg.Add(number_of_servers - 1)
+
+	for messages_received < number_of_servers {
+		// message := <-ch
+		// fmt.Println("Received message", message, "on server", serverId)
+
+		go readFullyFromClient(ch, my_records, &wg)
+		messages_received++
+
+	}
+	wg.Wait()
+	fmt.Println("Number of records on server", serverId, "is", len(my_records))
 
 }
 
-func ConnectToServer(host string, port string, server_id int) {
+func ConnectToServer(host string, port string, server_id int, file os.File) {
 	address := host + ":" + port
-	fmt.Println(" Connecting to server ", address, "from server", server_id)
+	//fmt.Println("Connecting to server ", address, "from server", server_id)
 	var conn net.Conn
 	var err error
 	for {
 		conn, err = net.Dial(Proto, address)
 		if err == nil {
 			// connected
+			fmt.Println("Connected to server ", address, "from server", server_id)
 			break
 		}
-		fmt.Println("Waiting for a second on server", server_id)
+		fmt.Println("Waiting for a second on ", server_id, "to connect to", address)
 		time.Sleep(1 * time.Second)
 	}
 
-	sendData(conn, server_id)
+	sendData(conn, server_id, file)
 
 }
 
-func sendData(conn net.Conn, server_id int) {
+func sendData(conn net.Conn, server_id int, file os.File) {
 	defer conn.Close()
 	var message string
 	if server_id == 1 {
